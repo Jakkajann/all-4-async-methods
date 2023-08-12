95674bc809391c8142f09387d0d68db4d571f983
Fernando Mocrosky <fernandobismaia@gmail.com>
2023-08-11 23:26:13 -0300

    All 4 complete without error handling

---
+++ script.js
@@ -0,0 +1,71 @@
+const url_endpoint = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+
+
+function getXHR() {
+    let XHR = new XMLHttpRequest();
+
+    XHR.onreadystatechange = () => {
+        if (XHR.status == 200 && XHR.readyState == 4) {
+            let data = JSON.parse(XHR.responseText)[0];
+            $("#quote").text(data);
+        }
+    }
+
+    XHR.open("GET", url_endpoint);
+    XHR.send();
+}
+
+
+function getFetch() {
+    function handleError(request) {
+        if(!request.ok) {
+            throw Error(request.status);
+        }
+        return request
+    }
+
+    fetch(url_endpoint)
+        .then(handleError)
+        .then((request) => {
+            return request.json();
+        })
+        .then((data)=> {
+            $("#quote").text(data[0]);
+        })
+        .catch((err)=> {
+            console.log("Error: " + err);
+        });
+}
+
+function getJqueryAjax() {
+    $.ajax({
+        method: "get",
+        url: url_endpoint,
+        dataType: "json"
+    })
+    .done((data) => {
+        $("#quote").text(data[0]);
+    })
+    .fail((err)=> {
+        console.log("Something went wrong: " + err);
+    })
+}
+
+function getAxios() {
+    axios.get(url_endpoint)
+        .then((res) => {
+            $("#quote").text(res.data[0]);
+        })
+        .catch((err)=> {
+            console.log("something went wrong: " + err);
+        })
+}
+
+$("#xhr").on("click", getXHR);
+
+$("#fetch").on("click", getFetch);
+
+$("#jquery").on("click", getJqueryAjax);
+
+$("#axios").on("click", getAxios);
